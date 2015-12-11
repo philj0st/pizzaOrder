5906d7fb56d7c14bea87a22a83e309ace161e630
Jost Philipp <Philipp.Jost@berufzug.ch>
2015-12-11 10:41:43 +0100

    implement the pizzaOrder with vue.js. remove grunt tasks to merge and open

--- dist/built.js
+++
@@ -1,3 +0,0 @@
-//Controller
-//Pizza.js
-//Selection.js contains related code to the <select> elements for example to select from a range of Pizzas
--- src/Selection.js
+++
@@ -1,1 +0,0 @@
-//Selection.js contains related code to the <select> elements for example to select from a range of Pizzas
